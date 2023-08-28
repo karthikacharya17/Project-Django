3acc56b71f6caf30f95ee1ada25a28e2db394f4f
Karthik <127333393+karthikacharya17@users.noreply.github.com>
2023-08-28 12:07:46 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,33 +1,14 @@
-from sqlite3 import Cursor
 from django.shortcuts import render
+from django.http import HttpResponse
 import mysql.connector as sql
-from django.contrib import messages
 from signup.models import signup
-
-
-
+from signup.views import signaction
 
 # Create your views here.
-def  signaction(request):
-    if request.method=="POST":
-        fname=request.POST.get('first_name',False)
-        lname=request.POST.get('last_name',False)
-        rno=request.POST.get('roll_no',False)
-        clas=request.POST.get('class',False)
-        semail=request.POST.get('email',False)
-        spas=request.POST.get('password',False)
-        if signup.objects.filter(email=semail).exists():
-            messages.info(request,'THIS EMAIL ALREDY EXIST!!!!')
+def  regaction(request):
+        k1 = signup.objects.all()
+        context = {'signup':k1}
         
+        return render(request,'reguser.html',context)
 
-        else:
-             ins=signup( firstname=fname,lastname=lname,rollno=rno,classs=clas,email=semail,password=spas)
-             ins.save()
-             messages.success(request,"Successfully Registered")
-    
 
-        
-        
-
-    return render(request,'signup_page.html')
-            
